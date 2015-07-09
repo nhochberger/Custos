3187ac3a713a192bcfbe2b00bfcb0465386d39e1
nhochberger <nico@irrwisch.net>
2015-07-09 22:25:21 +0200

    preparing system message area

---
+++ src/controller/SystemMessage.java
@@ -0,0 +1,13 @@
+package controller;
+
+import hochberger.utilities.eventbus.Event;
+
+public class SystemMessage implements Event {
+
+	@Override
+	public void performEvent() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/view/SystemMessageLabel.java
@@ -0,0 +1,17 @@
+package view;
+
+import hochberger.utilities.eventbus.EventReceiver;
+import controller.SystemMessage;
+
+public class SystemMessageLabel implements EventReceiver<SystemMessage> {
+
+	public SystemMessageLabel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void receive(final SystemMessage event) {
+		// TODO Auto-generated method stub
+	}
+}
