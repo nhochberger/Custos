8fd6781296db34c72ca18a573c1e14e316e218f8
nhochberger <nico@irrwisch.net>
2015-10-11 19:01:31 +0200

    - minor change in CustosMainFrame's layout - reformatting code in CustosApplication

--- src/controller/CustosApplication.java
+++ src/controller/CustosApplication.java
@@ -13,6 +13,7 @@ import modules.CustosModule;
 import modules.VisibleCustosModule;
 import modules.alarmclock.AlarmClock;
 import modules.clock.Clock;
+import modules.newsreader.NewsReader;
 import modules.weather.Weather;
 import view.ColorProvider;
 import view.CustosGui;
@@ -21,61 +22,73 @@ import controller.SystemMessage.MessageSeverity;
 
 public class CustosApplication extends BasicLoggedApplication {
 
-	private final Heartbeat heartbeat;
-	private final ScreenSaverProhibiter screenSaverProhibiter;
-	private final ColorProvider colorProvider;
-	private final BasicSession session;
-	private final CustosGui gui;
-	private final List<CustosModule> modules;
-	private final VersionChecker versionChecker;
-	private final SystemMessageMemory systemMessageMemory;
+    private final Heartbeat heartbeat;
+    private final ScreenSaverProhibiter screenSaverProhibiter;
+    private final ColorProvider colorProvider;
+    private final BasicSession session;
+    private final CustosGui gui;
+    private final List<CustosModule> modules;
+    private final VersionChecker versionChecker;
+    private final SystemMessageMemory systemMessageMemory;
 
-	public static void main(final String... args) {
-		setUpLoggingServices(CustosApplication.class);
-		try {
-			final ApplicationProperties applicationProperties = new ApplicationProperties();
-			final CustosApplication application = new CustosApplication(applicationProperties);
-			application.start();
-		} catch (final Exception e) {
-			getLogger().fatal("Error while starting application. Shutting down.", e);
-		}
-	}
+    public static void main(final String... args) {
+        setUpLoggingServices(CustosApplication.class);
+        try {
+            final ApplicationProperties applicationProperties = new ApplicationProperties();
+            final CustosApplication application = new CustosApplication(
+                    applicationProperties);
+            application.start();
+        } catch (final Exception e) {
+            getLogger().fatal(
+                    "Error while starting application. Shutting down.", e);
+        }
+    }
 
-	public CustosApplication(final ApplicationProperties applicationProperties) {
-		super();
-		this.session = new BasicSession(applicationProperties, new SimpleEventBus(), getLogger());
-		this.colorProvider = new DayTimeAwareColorProvider(this.session);
-		this.heartbeat = new Heartbeat(this.session);
-		this.screenSaverProhibiter = new ScreenSaverProhibiter(this.session);
-		this.versionChecker = new VersionChecker(this.session);
-		this.systemMessageMemory = new SystemMessageMemory();
-		this.session.getEventBus().register(this.systemMessageMemory, SystemMessage.class);
-		this.gui = new CustosGui(this.session, this.colorProvider, this.systemMessageMemory);
-		this.session.getEventBus().register(this.gui, HeartbeatEvent.class);
+    public CustosApplication(final ApplicationProperties applicationProperties) {
+        super();
+        this.session = new BasicSession(applicationProperties,
+                new SimpleEventBus(), getLogger());
+        this.colorProvider = new DayTimeAwareColorProvider(this.session);
+        this.heartbeat = new Heartbeat(this.session);
+        this.screenSaverProhibiter = new ScreenSaverProhibiter(this.session);
+        this.versionChecker = new VersionChecker(this.session);
+        this.systemMessageMemory = new SystemMessageMemory();
+        this.session.getEventBus().register(this.systemMessageMemory,
+                SystemMessage.class);
+        this.gui = new CustosGui(this.session, this.colorProvider,
+                this.systemMessageMemory);
+        this.session.getEventBus().register(this.gui, HeartbeatEvent.class);
 
-		this.modules = new LinkedList<>();
-		this.modules.add(new Clock(this.session, this.colorProvider));
-		this.modules.add(new Weather(this.session, this.colorProvider));
-		this.modules.add(new AlarmClock(this.session, this.colorProvider));
-		for (final CustosModule custosModule : this.modules) {
-			if (custosModule instanceof VisibleCustosModule) {
-				this.gui.addModule((VisibleCustosModule) custosModule);
-			}
-		}
-	}
+        this.modules = new LinkedList<>();
+        this.modules.add(new Clock(this.session, this.colorProvider));
+        this.modules.add(new Weather(this.session, this.colorProvider));
+        this.modules.add(new AlarmClock(this.session, this.colorProvider));
+        this.modules.add(new NewsReader(this.session, this.colorProvider));
+        for (final CustosModule custosModule : this.modules) {
+            if (custosModule instanceof VisibleCustosModule) {
+                this.gui.addModule((VisibleCustosModule) custosModule);
+            }
+        }
+    }
 
-	@Override
-	public void start() {
-		this.session.getEventBus().publish(new SystemMessage(MessageSeverity.SUCCESS, new DirectI18N("Starting application").toString()));
-		super.start();
-		for (final CustosModule custosModule : this.modules) {
-			this.session.getEventBus().register(custosModule, HeartbeatEvent.class);
-			custosModule.start();
-		}
-		this.gui.activate();
-		this.screenSaverProhibiter.start();
-		this.heartbeat.start();
-		this.versionChecker.start();
-		this.session.getEventBus().publish(new SystemMessage(MessageSeverity.SUCCESS, new DirectI18N("Custos ${0} successfully started.", this.session.getProperties().version()).toString()));
-	}
+    @Override
+    public void start() {
+        this.session.getEventBus().publish(
+                new SystemMessage(MessageSeverity.SUCCESS, new DirectI18N(
+                        "Starting application").toString()));
+        super.start();
+        for (final CustosModule custosModule : this.modules) {
+            this.session.getEventBus().register(custosModule,
+                    HeartbeatEvent.class);
+            custosModule.start();
+        }
+        this.gui.activate();
+        this.screenSaverProhibiter.start();
+        this.heartbeat.start();
+        this.versionChecker.start();
+        this.session.getEventBus().publish(
+                new SystemMessage(MessageSeverity.SUCCESS, new DirectI18N(
+                        "Custos ${0} successfully started.", this.session
+                                .getProperties().version()).toString()));
+    }
 }
--- src/view/CustosMainFrame.java
+++ src/view/CustosMainFrame.java
@@ -16,56 +16,65 @@ import controller.SystemMessageMemory;
 
 public class CustosMainFrame extends UndecoratedEDTSafeFrame {
 
-	private final List<VisibleCustosModule> modules;
-	private final ColorProvider colorProvider;
-	private final SystemMessageLabel systemMessageLabel;
-	private final SystemMessageDialog systemMessageDialog;
+    private final List<VisibleCustosModule> modules;
+    private final ColorProvider colorProvider;
+    private final SystemMessageLabel systemMessageLabel;
+    private final SystemMessageDialog systemMessageDialog;
 
-	public CustosMainFrame(final BasicSession session, final ColorProvider colorProvider, final SystemMessageMemory messageMemory) {
-		super(session.getProperties().title());
-		this.colorProvider = colorProvider;
-		this.modules = new CopyOnWriteArrayList<>();
-		this.systemMessageLabel = new SystemMessageLabel(colorProvider);
-		session.getEventBus().register(this.systemMessageLabel, SystemMessage.class);
-		this.systemMessageDialog = new SystemMessageDialog(colorProvider, messageMemory);
-		session.getEventBus().register(this.systemMessageDialog, SystemMessage.class);
-	}
+    public CustosMainFrame(final BasicSession session,
+            final ColorProvider colorProvider,
+            final SystemMessageMemory messageMemory) {
+        super(session.getProperties().title());
+        this.colorProvider = colorProvider;
+        this.modules = new CopyOnWriteArrayList<>();
+        this.systemMessageLabel = new SystemMessageLabel(colorProvider);
+        session.getEventBus().register(this.systemMessageLabel,
+                SystemMessage.class);
+        this.systemMessageDialog = new SystemMessageDialog(colorProvider,
+                messageMemory);
+        session.getEventBus().register(this.systemMessageDialog,
+                SystemMessage.class);
+    }
 
-	@Override
-	protected void buildUI() {
-		exitOnClose();
-		center();
-		frame().getContentPane().setBackground(this.colorProvider.backgroundColor());
-		useLayoutManager(new MigLayout("", "20[]:push[]:push[]20", "20[]push"));
-		frame().setAlwaysOnTop(true);
-		this.systemMessageLabel.build();
-		this.systemMessageDialog.build();
+    @Override
+    protected void buildUI() {
+        exitOnClose();
+        center();
+        frame().getContentPane().setBackground(
+                this.colorProvider.backgroundColor());
+        useLayoutManager(new MigLayout("debug, wrap 3", "20[]:push[]:push[]20",
+                "20[]20[]20[]push"));
+        frame().setAlwaysOnTop(true);
+        this.systemMessageLabel.build();
+        this.systemMessageDialog.build();
 
-		this.systemMessageLabel.getLabel().addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(final MouseEvent arg0) {
-				CustosMainFrame.this.systemMessageDialog.show();
-			}
-		});
+        this.systemMessageLabel.getLabel().addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(final MouseEvent arg0) {
+                CustosMainFrame.this.systemMessageDialog.show();
+            }
+        });
 
-		for (final CustosModule module : this.modules) {
-			add(module.getWidget().getComponent());
-		}
-		add(this.systemMessageLabel.getLabel(), "dock south, gapleft 5, gapright 5, gapbottom 5");
-		maximize();
-	}
+        for (final CustosModule module : this.modules) {
+            add(module.getWidget().getComponent());
+        }
+        add(this.systemMessageLabel.getLabel(),
+                "dock south, gapleft 5, gapright 5, gapbottom 5");
+        maximize();
+    }
 
-	public void addModuleToView(final VisibleCustosModule module) {
-		this.modules.add(module);
-	}
+    public void addModuleToView(final VisibleCustosModule module) {
+        this.modules.add(module);
+    }
 
-	public void update() {
-		if (!isBuilt()) {
-			return;
-		}
-		frame().getContentPane().setBackground(this.colorProvider.backgroundColor());
-		for (final VisibleCustosModule custosModule : this.modules) {
-			custosModule.updateWidget();
-		}
-	}
+    public void update() {
+        if (!isBuilt()) {
+            return;
+        }
+        frame().getContentPane().setBackground(
+                this.colorProvider.backgroundColor());
+        for (final VisibleCustosModule custosModule : this.modules) {
+            custosModule.updateWidget();
+        }
+    }
 }
